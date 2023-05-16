638993ef4c2dead83bf89a7740fc9b3c01f0f326
Karoline França <117689067+K4aro0lineFranca@users.noreply.github.com>
2023-05-16 07:59:46 -0300

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,17 @@
+function salvar(){
+    localStorage.Nome = document.getElementById('Nome').value
+    localStorage.Nascimento = document.getElementById('Nascimento').value
+    localStorage.NomeMae = document.getElementById('NomeMae').value
+    localStorage.NomePai = document.getElementById('NomePai').value
+    localStorage.catdd = document.getElementById('Contém algum tipo de doença').value
+    localStorage.Possuipassagempelapolicia = document.getElementById('Possui passagem pela policia').value
+    localStorage.Estado = document.getElementById('Estado').value
+    localStorage.Estadocivil = document.getElementById('Estado civil').value
+    localStorage.CPF = document.getElementById('CPF').value
+    localStorage.Endereço = document.getElementById('Endereço').value
+    localStorage.tele = document.getElementById('telefone').value
+    localStorage.City = document.getElementById('Cidade').value
+    localStorage.gde = document.getElementById('grau de escolaridade').value
+    localStorage.CEP = document.getElementById('CEP').value
+
+}
